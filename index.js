4ade890c5b4f5dee169e40ec28496542e152716b
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-09-07 13:55:59 +0200

    👌 IMPROVE: fix bug

--- index.js
+++ index.js
@@ -51,29 +51,3 @@ $(document).ready(function () {
   });
 
 });
-
-function initMap() {
-  $("#map-canvas").show();
-  console.log("Type Of Google: " + typeof google);
-  var geocodeString = $("#map-canvas").data("geocode");
-  var geocode = geocodeString.split(',');
-  var myLatlng = new google.maps.LatLng(parseFloat(geocode[0]), parseFloat(geocode[1]));
-
-  var myOptions = {
-    zoom: 18,
-    center: myLatlng,
-    mapTypeControl: true,
-    mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
-    navigationControl: true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  }
-
-  var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    title: "Hello World!"
-  });
-
-  // To add the marker to the map, call setMap();
-  marker.setMap(map);
-}
